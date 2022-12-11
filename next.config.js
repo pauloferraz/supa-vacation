e60137a18d6c20775f0bc6d3c6553b3344ee9d06
pauloferraz <dev.paulo.ferraz@gmail.com>
2022-12-11 17:36:54 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'supabase.in',
+				port: '',
+				pathname: '/**'
+			}
+		],
+		domains: ['ynjyenizjfpifjniaetu.supabase.in', 'lh3.googleusercontent.com']
+	}
+};
